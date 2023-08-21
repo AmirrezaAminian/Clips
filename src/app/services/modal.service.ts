84db5c7f91742e3f861e419e091d81e9cc02ac9e
AmirrezaAminian <aminian.amirreza@gmail.com>
2023-08-21 14:07:00 +0330

    closing the modal

--- src/app/services/modal.service.ts
+++ src/app/services/modal.service.ts
@@ -5,7 +5,7 @@ import { Injectable } from '@angular/core';
 })
 
 export class ModalService {
-  private visible = false ;
+  public  visible = false ;
 
   constructor( ) { }
 
--- src/app/shared/modal/modal.component.ts
+++ src/app/shared/modal/modal.component.ts
@@ -15,6 +15,10 @@ export class ModalComponent {
      
   }
 
+  closeModal(){
+      this.modal.toggleModal()
+  }
+
 }
 
 
