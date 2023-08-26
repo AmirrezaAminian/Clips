e329b83667bd7b09d71c935ccf0bf26eacc24219
AmirrezaAminian <aminian.amirreza@gmail.com>
2023-08-26 16:16:02 +0330

    032 Setting an Active Tab

--- src/app/shared/tab/tab.component.ts
+++ src/app/shared/tab/tab.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component , Input } from '@angular/core';
 
 @Component({
   selector: 'app-tab',
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./tab.component.css']
 })
 export class TabComponent {
+  @Input() tabTitle = ''
+  @Input() active = false
+
 
 }
--- src/app/shared/tabs-container/tabs-container.component.ts
+++ src/app/shared/tabs-container/tabs-container.component.ts
@@ -1,16 +1,33 @@
-import { Component , AfterContentInit , ContentChildren , QueryList } from '@angular/core';
+import {
+  Component,
+  AfterContentInit,
+  ContentChildren,
+  QueryList,
+} from '@angular/core';
 import { TabComponent } from '../tab/tab.component';
 
 @Component({
   selector: 'app-tabs-container',
   templateUrl: './tabs-container.component.html',
-  styleUrls: ['./tabs-container.component.css']
+  styleUrls: ['./tabs-container.component.css'],
 })
 export class TabsContainerComponent implements AfterContentInit {
-  @ContentChildren(TabComponent) tabs : QueryList<TabComponent> = new QueryList()
+  @ContentChildren(TabComponent) tabs: QueryList<TabComponent> =
+    new QueryList();
 
-   ngAfterContentInit(): void {
-     console.log(this.tabs);
-     
-   }
+  ngAfterContentInit(): void {
+    const activeTabs = this.tabs?.filter((tab) => tab.active);
+
+    if (!activeTabs || activeTabs.length === 0) {
+      this.selectTab(this.tabs.first);
+    }
+  }
+
+  selectTab(tab  : TabComponent){
+    this.tabs?.forEach(tab => {
+      tab.active  = false 
+    })
+
+    tab.active = true ;
+  }
 }
